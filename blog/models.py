323994d9fec0b95d33787c644064b29ae06cb152
piotr <piotr.paluch@outlook.com>
2022-11-14 14:37:49 +0100

    Dodawanie modelu obrazów, modyfikacja blog.css i settings.py

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True,null=True)
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
 
     def publish(self):
         self.published_date = timezone.now()
